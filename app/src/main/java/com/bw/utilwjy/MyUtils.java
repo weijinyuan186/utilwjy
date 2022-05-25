a7e9f13a962e7235e6ae24cf3c6d0ee6d260df59
魏靳元 <2670478006@qq.com>
2022-05-25 14:02:36 +0800

    first commit

---
+++ app/src/main/java/com/bw/utilwjy/MyUtils.java
@@ -0,0 +1,16 @@
+package com.bw.utilwjy;
+
+/**
+ * @ClassName: MyUtil$
+ * @Description: java类作用描述
+ * @Author: 魏靳元
+ * @CreateDate: 2022/5/25 09:08
+ * @UpdateRemark: 更新内容
+ * @Version: 1.0
+ */
+public class MyUtils {
+
+    public static int test(){
+        return 8;
+    }
+} 
